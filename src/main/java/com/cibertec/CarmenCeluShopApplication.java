b1e1b62132fd25f6fb0bb34aee08038db6c9b7e9
carmen flores <cpft1998@gmail.com>
2023-09-30 11:20:07 -0500

    inicio de proyecto

---
+++ src/main/java/com/cibertec/CarmenCeluShopApplication.java
@@ -0,0 +1,13 @@
+package com.cibertec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarmenCeluShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarmenCeluShopApplication.class, args);
+	}
+
+}
